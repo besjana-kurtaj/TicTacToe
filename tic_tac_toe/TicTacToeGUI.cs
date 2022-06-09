e610010433fd8dbbb379f8788172e1789c81f093
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2022-06-09 19:19:35 +0200

    TicTacToeGUI

--- tic_tac_toe/TicTacToeGUI.cs
+++ tic_tac_toe/TicTacToeGUI.cs
@@ -5,6 +5,28 @@ namespace tic_tac_toe
 {
   public partial class TicTacToeGUI : Form
   {
+     GameBoard gb;
+
+    public TicTacToeGUI()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      gb = new TicTacToeBoard();
+      LoadBoard();
+      button1.Enabled = true;
+      button2.Enabled = true;
+      button3.Enabled = true;
+      button4.Enabled = true;
+      button5.Enabled = true;
+      button6.Enabled = true;
+      button7.Enabled = true;
+      button8.Enabled = true;
+      button9.Enabled = true;
+
+    }
    
   }
 }
