615d4f7ffa0c78540931347fef8489da122fa5a4
MihrijeIbrahimi <75813395+MihrijeIbrahimi@users.noreply.github.com>
2022-06-09 10:30:33 -0700

    Update GameBoard.cs

--- tic_tac_toe/GameBoard.cs
+++ tic_tac_toe/GameBoard.cs
@@ -8,10 +8,48 @@ namespace tic_tac_toe
     O = -1,
     Open = 0,
   }
-
+ 
   public abstract class GameBoard
   {
-   
-  
+
+    /// Array that contains all of the spaces on the board.
+    public Player[,] squares;
+
+    /// Gets or sets a space on the board.
+    public abstract Player this[int x, int y] { get; set; }
+
+    /// Determines if all spaces on the board are full.
+    public abstract bool IsFull { get; }
+
+
+    /// Gets the maximum size of the board.
+    public abstract int Size { get; }
+
+
+    /// List of the open spaces available on the current board.
+    public abstract List<Space> OpenSquares { get; }
+
+
+    /// Determines if there is a winner on the current board.
+    public abstract Player Winner { get; }
+
+ 
+    /// Makes a deap copy of the current board
+    public abstract GameBoard Clone();
+  }
+
+  /// Describes a space on the board.
+    public struct Space
+  {
+    public int X;
+    public int Y;
+    public double Rank;
+
+    public Space(int x, int y)
+    {
+      this.X = x;
+      this.Y = y;
+      Rank = 0;
+    }
   }
 }
